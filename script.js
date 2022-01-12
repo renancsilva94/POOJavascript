7bde7bd68bc6356c8d158275a1e8243d958bf6ba
renancsilva94 <renancsilva94@gmail.com>
2022-01-12 16:43:37 -0300

    conclusão exercicio POO

---
+++ script.js
@@ -0,0 +1,74 @@
+class ContaBancaria {
+  constructor(agencia, numero, tipo){
+      this.agencia = agencia; 
+      this.numero = numero;
+      this.tipo = tipo;
+      this.saldo = 0;
+  }
+
+  get saldo(){
+    return this._saldo;
+  }
+
+  set saldo(valor){
+    this._saldo = valor;
+  }
+  sacar(valor){
+   if (valor > this._saldo){
+      return 'Operação negada';
+   }
+        this._saldo = this._saldo - valor;
+          return this._saldo;
+  }
+
+  depositar (valor) { 
+    this._saldo =  this._saldo + valor;
+      return this._saldo;
+  }
+}
+
+
+
+class ContaCorrente extends ContaBancaria {
+  constructor(agencia, numero){
+    super(agencia, numero); //tudo que receber vai para o construtor;
+    this.tipo = 'corrente';
+    this.cartaoCredito = cartaoCredito;
+  } 
+
+  get cartaoCredito(){
+    return this._cartaoCredito;
+  }
+
+  set cartaoCredito(valor){
+     this._cartaoCredito = valor; 
+  }
+}
+
+
+
+class ContaPoupança extends ContaBancaria {
+  constructor(agencia, numero){
+    super(agencia, numero); //tudo que receber vai para o construtor;
+    this.tipo = 'poupança';
+  }
+}
+
+
+
+class ContaUniversitaria extends ContaBancaria {
+  constructor(agencia, numero){
+    super(agencia, numero); //tudo que receber vai para o construtor;
+    this.tipo = 'universitaria';
+  }
+
+  sacar(valor){
+    if (valor > 500){
+      return 'Operação negada';
+    }
+    this._saldo = this._saldo - valor;
+    return this._saldo;
+  }
+}
+
+
